e40242f74ced96918cd82c76b590ba77d44f417a
gigib734 <gblo0002@student.monash.edu>
2026-08-22 16:49:42 +1000

    vsd

---
+++ GSE113165.R
@@ -0,0 +1,34 @@
+library(readxl)
+library(DESeq2)
+library(dplyr)
+
+pheno <- read.csv("C:/Users/gbloc/Desktop/PCDHG_Gila/Transcriptomic rna seq data/GSE113165/GSE113165 pheno.csv")
+counts <- read.delim("C:/Users/gbloc/Desktop/PCDHG_Gila/Transcriptomic rna seq data/GSE113165/GSE113165_counts.txt")
+
+head(counts)
+head(pheno)
+head(pheno_raw)
+unique(pheno$Sex)
+
+coldata <- pheno
+coldata$age <- as.factor(coldata$age)
+
+dds <- DESeqDataSetFromMatrix(round(as.matrix(counts)), colData = coldata, design = ~ age + Sex)
+dds1 <- DESeq(dds)
+res <- results(dds)
+
+df_results <- as.data.frame(result)
+rownames(df_results) <- res@rownames
+head(df_results)
+
+# Map Ensembl to gene symbols 
+gtf<- file.path(df_results, "Human.GRCh38.p14.gtf")
+gtf_df<- as.data.frame(rtracklayer::import(gtf))
+
+library(rtracklayer)
+gtf <- "C:/Users/gbloc/OneDrive/Desktop/PCDHG_Gila/Coding/Human.GRCh38.p14.gtf"
+
+
+
+save(df_results,file = )
+write.csv(df_results, file ="C:/Users/gbloc/OneDrive/Desktop/PCDHG_Gila/Coding/DALE_metanalysis")
